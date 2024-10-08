f5e65cb96156e4f58af39d75da401500f35cbd18
Alertas Locas <49847759+Gerez24@users.noreply.github.com>
2024-10-08 20:35:19 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const audio = document.getElementById('radio');
+const playPauseBtn = document.getElementById('playPause');
+const volumeSlider = document.getElementById('volume');
+
+// Play/Pause Functionality
+playPauseBtn.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        playPauseBtn.classList.remove('paused');
+    } else {
+        audio.pause();
+        playPauseBtn.classList.add('paused');
+    }
+});
+
+// Volume Control
+volumeSlider.addEventListener('input', () => {
+    audio.volume = volumeSlider.value;
+});
